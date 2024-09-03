c89fa85cc5abeb3378d1607bf44d3be93995d9eb
Keren Rojas <233408@ids.upchiapas.edu.mx>
2024-09-03 22:32:53 +0800

    first commit

---
+++ src/page/Home.tsx
@@ -0,0 +1,12 @@
+import CharacterList from "../Components/organisms/CharacterList";
+
+function Home() {
+  return(
+    <div>
+      <h1>Dragon Ball</h1>
+      <CharacterList></CharacterList>
+    </div>
+  )
+}
+
+export default Home;
